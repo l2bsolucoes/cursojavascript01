3a70cf39aa964b608271c2f648312aa07e541c14
Leslie Bagalho <contato@l2bsolucoes.com.br>
2023-09-30 18:15:23 -0300

    criando a função no js

--- 04calculadoraflex/js/main.js
+++ 04calculadoraflex/js/main.js
@@ -3,4 +3,25 @@
  * @author Leslie Bagalho
  */
 
-/*alert("teste"); use esse comando para ter certeza de que o JS está funcionando*/
+//alert("teste"); use esse comando para ter certeza de que o JS está funcionando
+
+var etanol, gasolina;
+
+function calcular(){
+    /*parseFloat converte o que esta no parenteses em numeros decimais*/
+    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",",".")); 
+    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",","."));
+
+    if(etanol < 0.7 * gasolina){
+        //verdadeiro
+        document.getElementById("status").src="/cursojavascript01/04calculadoraflex/img/etanol.png";
+        
+    }else{
+        //falso
+        document.getElementById("status").src="/cursojavascript01/04calculadoraflex/img/gasolina.png";
+    }
+}
+
+function resetar(){
+    document.getElementById("status").src="/cursojavascript01/04calculadoraflex/img/neutro.png";
+}
